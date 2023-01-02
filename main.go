cd11b2fa6ed3498d2bf4c541ac45c8ddd090d9d4
--global <smilewendy7@gmail.com>
2023-01-02 10:42:57 -0500

    03_create the first block

--- main.go
+++ main.go
@@ -41,14 +41,40 @@ func (block *Block) SetHash() {
 	block.Hash = hash[:]
 }
 
-//四. 引入区块链
+// 四. 引入区块链
+// 把所有数据穿起来-数组
+type BlockChain struct {
+	//当以一个区块链数组
+	blocks []*Block
+}
+
+// 五. 定义一个区块链
+func NewBlockChain() *BlockChain {
+	// 一般定义区块链要往里边塞上一块儿糖
+	// 创建一个创世块，并作为第一个区块添加到区块链中
+	genesisBlock := GenesisBlock()
+	return &BlockChain{
+		blocks: []*Block{genesisBlock},
+	}
+}
+
+// 创世块
+func GenesisBlock() *Block {
+	return NewBlock("GO一期创世块， 老牛逼了！", []byte{})
+
+}
+
 //五. 添加区块
 //六. 重构代码
 
 func main() {
-	block := NewBlock("老师转班长一枚比特币", []byte{})
-	fmt.Printf("前区块哈希值： %x\n", block.PrevHash)
-	fmt.Printf("当前区块哈希值： %x\n", block.Hash)
-	fmt.Printf("区块数据： %s\n", block.Data)
+	bc := NewBlockChain()
+	for i, block := range bc.blocks {
+		fmt.Printf("======= 当前前区块高度： %d======\n", i)
+		fmt.Printf("前区块哈希值： %x\n", block.PrevHash)
+		fmt.Printf("当前区块哈希值： %x\n", block.Hash)
+		fmt.Printf("区块数据： %s\n", block.Data)
+
+	}
 
 }
