6c6630f8825cffc3a5389afb401e29aea83cbbdb
--global <smilewendy7@gmail.com>
2023-01-02 10:13:12 -0500

    02_SetHash

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+)
 
 // 0. 定义结构
 type Block struct {
@@ -17,15 +20,27 @@ type Block struct {
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := Block{
 		PrevHash: prevBlockHash,
-		Hash:     []byte{}, // 先定义空， 再后边计算 // TODO
+		Hash:     []byte{}, // 先定义空， 再后边计算
 		Data:     []byte(data),
 	}
+
+	block.SetHash()
 
 	return &block
 
 }
 
-//三. 生成哈希
+// 三. 生成哈希
+// 给block 注册一个函数 ***
+func (block *Block) SetHash() {
+	// block 内部做hash
+	// 1. 拼装数据
+	blockInfo := append(block.PrevHash, block.Data...)
+	// 2. sha256
+	hash := sha256.Sum256(blockInfo)
+	block.Hash = hash[:]
+}
+
 //四. 引入区块链
 //五. 添加区块
 //六. 重构代码
