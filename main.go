0f3e0362063c2986640e4e38930fe0f99d382409
--global <smilewendy7@gmail.com>
2023-01-04 18:27:37 -0500

    03- create blcok chain structure

--- main.go
+++ main.go
@@ -40,18 +40,47 @@ func (block *Block) calHash() {
 	block.Hash = hash[:]
 }
 
-// -- 给block
+// 4. 引入区块链  - 一组区块们
 
-// 4. 引入区块链 ？？
+// 定义区块链结构
+type BlockChain struct {
+	blocks []*Block
+}
+
+// 定义一个区块链
+func newBlockChain() *BlockChain {
+	return &BlockChain{
+		// 第一块是创世块
+		blocks: []*Block{GenesisBlock()},
+	}
+
+}
+
+// 第一块- 创世块
+func GenesisBlock() *Block {
+	//NewBlock(prevBlockHash []byte, data string)*Block
+	genBlock := NewBlock([]byte{}, "韭菜币的诞生")
+	return genBlock
+
+}
 
 //5.  添加区块
+//a. 创建新的区块
+//b. 添加到区块链中
 
 // 升级版
 
 func main() {
-	block := NewBlock([]byte{}, "Bronson transfer Wendy 500 dollars")
-	fmt.Printf("前一区块是 %x\n", block.PrevHash)
-	fmt.Printf("当前区块是 %x\n", block.Hash)
-	fmt.Printf("区块数据 %s\n", block.Data)
+	//block := NewBlock([]byte{}, "Bronson transfer Wendy 500 dollars")
+
+	chain := newBlockChain()
+
+	for i, block := range chain.blocks {
+		fmt.Printf("==== 当前层高为 %d\n====", i)
+		fmt.Printf("前一区块哈希值是 %x\n", block.PrevHash)
+		fmt.Printf("当前区块哈希值是 %x\n", block.Hash)
+		fmt.Printf("区块数据 ： %s\n", block.Data)
+
+	}
 
 }
